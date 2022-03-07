367958d5049f0dce02b132925ae962930973eb2c
Sean <larcenciel12@gmail.com>
2022-03-07 17:02:32 +0800

    test

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ app.use("/", router);
 
 const initServer = async () => {
 
-
+  router.post('/',(req, res) => {
+    console.log(req.body);
+    res.end('ok');
+  });
   
 
   const response = await fetch('https://api.sportradar.com/soccer-extended/trial/v4/stream/events/subscribe?api_key=uxntnnupmr3228nuxswaa77x&format=json&sport_event_id=sr:sport_event_id:32382675', {method: 'GET'});
   const data = await response.json();
   console.log(data);
-  router.post('/',(req, res) => {
-    console.log(req.body);
-    res.end('ok');
-  });
+
 
   app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
  
