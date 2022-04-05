84f26c0c196416559aedc176b3f16c4d7e87230f
Sean <larcenciel12@gmail.com>
2022-04-05 18:02:14 +0800

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import fetch from 'node-fetch';
 const app = express();
 const PORT = process.env.PORT || 3003;
 const router = express.Router();
+
 app.use("/", router);
 
 const initServer = async () => {
@@ -12,11 +13,17 @@ const initServer = async () => {
     console.log(req.body);
     res.end('ok');
   });
-  
-
+  countStream.on('total',(count)=>{
+    console.log(count)
+  })
+  router.get('/',(req, res) => {
+    console.log(req.body);
+    res.pipe(countStream);
+    res.end('ok');
+  });
   const response = await fetch('https://api.sportradar.com/soccer-extended/trial/v4/stream/events/subscribe?api_key=uxntnnupmr3228nuxswaa77x&amp;format=json&amp;sport_event_id=sr:sport_event_id:5840253', {method: 'GET'});
   
-  console.log(response);
+  response.pipe(countStream);
 
 
   app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
