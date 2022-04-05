998be594187c1487de95834951fc92ac5a507089
Sean <larcenciel12@gmail.com>
2022-04-05 19:05:34 +0800

    test

--- index.js
+++ index.js
@@ -20,14 +20,13 @@ const initServer = async () => {
   .then(
   res =>
     new Promise((resolve, reject) => {
-      const dest = fs.createWriteStream("./tmp.txt");
-      res.body.pipe(dest);
+      
       res.body.pipe((e)=>{
         console.log(e);
       });
       
       res.body.on("end", () => resolve("it worked"));
-      dest.on("error", reject);
+    
 
 
       
